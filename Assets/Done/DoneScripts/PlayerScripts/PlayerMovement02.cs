30c2f8387b9b8b73f32a1961fbb886f64b5240be
CheckPi <a630042336@live.com>
2017-06-22 19:52:10 +0800

    众多八向跑效果（资源提交），能动的都试了
    AnimatorController说明
    AnimKids_withStealthAnim_1算是八向跑了，八个方向都是直线，但转向特别奇怪，因为用了stealth里的转身动画
    AnimKids_withStealthAnim_2也算八向跑，斜向跑不是直线的，是绕了一个大弯，不明显而已
    AnimKids_withStealthAnim_1/2都是用了stealth带实际位移的动画，只是WalkRun状态机的混合方式不同而已
    
    AnimKids_withTTAnim_1是用美术大大提供的动画（不带实际位移），没什么问题，就是感觉跑步动作不够大不够明显，但与AnimWithTTTranslateStyle和AnimWithTTCControllerStyle搭配时人物会越跑越升高（即双脚离地），直到y=0.4左右
    
    测试情况（下列情况在DoneCameraMovement的normalStyle进行，thirdPersonCamera打死都不要用了，美术的CameraMouseOrbit应该也可以吧暂时没发现问题；摄像机我不懂……到时可能要量身订造写一个了…………）
    
    使用AnimKids_withStealthAnim_1/2的情况下
    
    AnimWithStealthOffsetStyle与Anim_1/2搭配，往某个方向都还好，但转向时Anim_1会停顿，Anim_2会像酒鬼那样晃几下
    
    使用AnimKids_withTTAnim_1的情况下
    
    AnimWithTTTranslateStyle用了Translate实现八向跑（严格来说是漂浮），但没有转向……因为旋转后自身坐标系发生变化，我就不懂怎么写了……
    AnimWithTTCControllerStyle用Character.move函数也实现了八向跑，有时会滑动
    
    AnimWithTTRigidVelocityStyle（使用时最好禁用CharacterController组件，不然有可能跑不了）算是最流畅的，转向也很方便，比较好
    
    最后，偶然发现AnimKids_withStealthAnim_2与AnimWithTTRigidVelocityStyle似乎是效果最好的，转向最方便，最有奔跑的感觉=
    =
    
    bug
    禁用CharacterController后，主角可以直接穿过Cube……

---
+++ Assets/Done/DoneScripts/PlayerScripts/PlayerMovement02.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ACStyle
+{
+    AnimWithStealthOffsetStyle,
+    AnimWithTTTranslateStyle,
+    AnimWithTTRigidVelocityStyle,
+    AnimWithTTCControllerStyle
+}
+
+public class PlayerMovement02 : MonoBehaviour
+{
+
+    Animator anim;              // Reference to the animator component.
+    DoneHashIDs hash;			// Reference to the HashIDs.
+    CharacterController characteController;  // Reference to the CharacterController component.
+    Rigidbody rigidbody;
+
+    public ACStyle acStyle = ACStyle.AnimWithStealthOffsetStyle;
+    public float turnSmoothing = 15f;	// A smoothing value for turning the player.
+    public float movePower = 2f;
+    public float speedDampTime = 0.1f;
+    public float directionDampTime = 0.1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Setting up the references.
+        anim = GetComponent<Animator>();
+        hash = GameObject.FindGameObjectWithTag(DoneTags.gameController).GetComponent<DoneHashIDs>();
+        rigidbody = GetComponent<Rigidbody>();
+        characteController = GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate()
+    {
+        // Cache the inputs.
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        switch (acStyle)
+        {
+            case ACStyle.AnimWithStealthOffsetStyle:
+                OffsetMovementManagement(h, v);
+                break;
+            case ACStyle.AnimWithTTTranslateStyle:
+                NoOffsetTranslateMovement(h, v);
+                break;
+            case ACStyle.AnimWithTTRigidVelocityStyle:
+                NoOffsetRigidVelocityMovement(h, v);
+                break;
+            case ACStyle.AnimWithTTCControllerStyle:
+                NoOffsetCControllerMovement(h, v);
+                break;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //AudioManagement
+    }
+
+    void OffsetMovementManagement(float horizontal, float vertical)
+    {
+        Vector3 worldDir;
+        Vector3 dir = new Vector3(horizontal, 0, vertical);
+        //worldDir = Camera.main.transform.rotation * dir;
+        worldDir = dir;
+        worldDir.y = 0;
+        worldDir.Normalize();
+
+        float speed = Mathf.Clamp(worldDir.magnitude, 0, 1);
+        float direction = 0.0f;
+
+        if (speed > 0.01f)
+        {
+            Vector3 axis = Vector3.Cross(transform.forward, worldDir);
+            direction = Vector3.Angle(transform.forward, worldDir) / 180.0f * (axis.y < 0 ? -1 : 1);
+        }
+        UpdateSpeedParam(speed * 6, speedDampTime);
+        UpdateDirectionParam(direction * 180, directionDampTime);
+    }
+
+    // 运用此模式需开启CharacterController组件，无需禁用rigidbody组件
+    void NoOffsetCControllerMovement(float horizontal, float vertical)
+    {
+        Vector3 moveDir = new Vector3(horizontal * movePower, 0, vertical * movePower);
+        characteController.Move(moveDir * Time.deltaTime);
+
+        // If there is some axis input...
+        if (horizontal != 0f || vertical != 0f)
+        {
+            // ... set the players rotation and set the speed parameter to 5.5f.
+            Rotating(horizontal, vertical);         
+        }
+
+        float speed = Mathf.Clamp(moveDir.magnitude, 0, 1);
+        UpdateSpeedParam(speed * 6, speedDampTime);
+    }
+
+    void NoOffsetTranslateMovement(float horizontal, float vertical)
+    {
+        Vector3 moveDir = new Vector3(horizontal * movePower, 0, vertical * movePower);
+
+        transform.Translate(moveDir * Time.deltaTime);
+        /* 这里应该有主角面向旋转才对，但一转本地坐标系就发生变化，
+         * 行走方向就又变了，不再是原来一开始瞄准的方向
+        // If there is some axis input...
+        if (horizontal != 0f || vertical != 0f)
+        {
+            // ... set the players rotation and set the speed parameter to 5.5f.
+            Rotating(horizontal, vertical);
+        }
+        */
+
+        float speed = Mathf.Clamp(moveDir.magnitude, 0, 1);
+        UpdateSpeedParam(speed * 6, speedDampTime);
+    }
+
+    // 运用此模式需禁用CharacterController组件
+    void NoOffsetRigidVelocityMovement(float horizontal, float vertical)
+    {
+        
+        Vector3 moveDir = new Vector3(horizontal * movePower, 0, vertical * movePower);
+        if (horizontal != 0f || vertical != 0f)
+        {
+            rigidbody.velocity = moveDir;
+            Rotating(horizontal, vertical);
+        }
+           
+        float speed = Mathf.Clamp(moveDir.magnitude, 0, 1);
+        UpdateSpeedParam(speed * 6, speedDampTime);
+    }
+
+
+    // 更新Speed动画参数
+    void UpdateSpeedParam(float speed, float speedDampTime)
+    {
+        anim.SetFloat(hash.speedFloat, speed, speedDampTime, Time.deltaTime);
+    }
+
+    // 更新Direction动画参数
+    void UpdateDirectionParam(float direction, float directionDampTime)
+    {
+        anim.SetFloat(hash.directionFloat, direction, directionDampTime, Time.deltaTime);
+    }
+
+    void Rotating(float horizontal, float vertical)
+    {
+        // Create a new vector of the horizontal and vertical inputs.
+        Vector3 targetDirection = new Vector3(horizontal, 0f, vertical);
+
+        // Create a rotation based on this new vector assuming that up is the global y axis.
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+
+        // Create a rotation that is an increment closer to the target rotation from the player's rotation.
+        Quaternion newRotation = Quaternion.Lerp(rigidbody.rotation, targetRotation, turnSmoothing * Time.deltaTime);
+
+        // Change the players rotation to this new rotation.
+        rigidbody.MoveRotation(newRotation);
+    }
+}
